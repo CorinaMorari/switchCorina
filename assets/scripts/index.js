f3a0aa7382861e21b8ae4ddc9980c6761252e250
Corina <morarik8@gmail.com>
2022-11-01 17:06:53 +0200

    Done project

---
+++ assets/scripts/index.js
@@ -0,0 +1,25 @@
+'use strict'
+const body = document.querySelector('body')
+const divParent = document.createElement('div');
+const switchDiv = document.createElement('label');
+const inputCheckbox = document.createElement('input');
+const spanSlider = document.createElement('span');
+
+
+switchDiv.classList.add('switch');
+switchDiv.setAttribute('for','idCheckbox');
+inputCheckbox.classList.add('checkbox')
+inputCheckbox.setAttribute('type','checkbox');
+inputCheckbox.setAttribute('id','idCheckbox');
+spanSlider.classList.add('slider')
+spanSlider.classList.add('round')
+
+
+body.append(switchDiv);
+switchDiv.append(inputCheckbox);
+switchDiv.append(spanSlider);
+
+inputCheckbox.addEventListener('change', () =>{
+    body.classList.toggle('dark');
+})
+
